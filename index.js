44da77255d552513e74bb3871f4a89a002fb0237
gettyreal <getty.fafano@gmail.com>
2024-10-07 10:32:27 +0200

    svg changes complete

--- index.js
+++ index.js
@@ -7,28 +7,3 @@ fetch(url)
         document.getElementById('profile-name').textContent = data.login || 'No Name Available';
     })
     .catch(error => console.error('Error fetching data:', error));
-/*
-const home = document.getElementById('home');
-const projects = document.getElementById('projects');
-const contsct = document.getElementById('contact');
-
-function homeLink() {
-    home.classList.add('linkActive');
-    projects.classList.remove('linkActive');
-    contact.classList.remove('linkActive');
-
-}
-
-function projectsLink() {
-    projects.classList.add('linkActive');
-    home.classList.remove('linkActive');
-    contact.classList.remove('linkActive');
-
-}
-
-function contactLink () {
-    contact.classList.add('linkActive');
-    projects.classList.remove('linkActive');
-    home.classList.remove('linkActive');
-}
-*/
